ce409435e1e922611c25cee5eb68ef26ea91905c
JAIR NAZARENO <jairnazareno12@gmail.com>
2020-12-15 16:47:42 -0500

    mitad del deber

--- CapaConsola/Program.cs
+++ CapaConsola/Program.cs
@@ -15,18 +15,23 @@ namespace CapaConsola
             int mes;
 
             CapaLogica.Contabilidad conta = new CapaLogica.Contabilidad();
+
             Console.WriteLine("Ingrese los siguientes valores");
             Console.WriteLine("Saldo Inicial: ");
+
             saldo = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Su nombre: ");
             nombre = Console.ReadLine();
+
             Console.WriteLine("Numero del mes: ");
             mes = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Fecha: ");
             fecha = Console.ReadLine();
-            Console.WriteLine("\nResultado del sueldo {0}",conta.getSaldos(saldo);
+
+            Console.WriteLine("\nResultado del sueldo {0}", conta.getSaldos(saldo));
             Console.WriteLine(conta.Saludos(nombre));
             Console.WriteLine(CapaLogica.Utilidades.getNombreDelMes(mes));
+
             Console.ReadKey();
         }
     }
